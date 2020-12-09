86ab4cd1217453b056ed97c71b34bd0e5d92a577
crawfmas <56137534+crawfmas@users.noreply.github.com>
2020-12-08 22:44:46 -0800

    oh, so no HEAD?
    I don't know this is but its everywhere and I don't like it
    <<<<<<< HEAD
    
    =======
      
    >>>>>>> eabf42d6b54ca13927364871dc949a6956cf032f

--- public/realIndex.js
+++ public/realIndex.js
@@ -3,21 +3,10 @@ var signInButton = document.getElementById("sign-in-button")
 
 tutorFinderButton.addEventListener('click', function(){
   console.log("== Tutor finder was clicked");
-<<<<<<< HEAD
   window.location.href = '/tutorSearch';
-=======
-  window.location.href = 'tutors';
->>>>>>> eabf42d6b54ca13927364871dc949a6956cf032f
 });
 
 signInButton.addEventListener('click', function(){
   console.log("== Sign in was clicked");
-<<<<<<< HEAD
   window.location.href = 'tutors.html';
 });
-=======
-  window.location.href = 'tutors';
-});
-
-
->>>>>>> eabf42d6b54ca13927364871dc949a6956cf032f
