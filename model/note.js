3325d46c204f6b87e86b9548f5818caaec56f1a2
2333tracy <58852317+2333tracy@users.noreply.github.com>
2020-12-09 22:49:04 -0800

    Add files via upload

---
+++ model/note.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose');
+
+// connect mongoDb
+mongoose.connect('mongodb://localhost/test3', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+var Schema = mongoose.Schema;
+
+var blogSchema = new Schema({
+    tutor: String,
+    major: String,
+    imgSrc: String,
+    grade: String,
+    body: String,
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+var Blog = mongoose.model('Blog', blogSchema);
+
+
+module.exports = Blog
